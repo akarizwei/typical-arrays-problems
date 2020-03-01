d592407f3fb6863fe7732bebc0e96bdfb3805a20
Akarizwei <andy.b.sky@yandex.ru>
2020-03-01 19:20:05 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = undefined) {
+    if (array == undefined || array.length == 0){
+        return 0
+    }
+    let min = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = undefined) {
+    if (array == undefined || array.length == 0){
+        return 0
+    }
+    let max = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = undefined) {
+    if (array == undefined || array.length == 0){
+        return 0
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (array.length === 0 || array === undefined){
+        return 0
+    }
+  return array.reduce(reducer)/array.length;
 }
